86e8fd70a915b937c056e979c0c010164bb4263b
Scott Kauker <swkauker@gmail.com>
2016-07-24 23:37:58 -0700

    Begin implementing wallcmd command line utility

---
+++ wallcmd/Options.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using CommandLine;
+
+namespace wallcmd
+{
+
+    class SetOptions
+    {
+        [Option('n', "name", HelpText = "Window name to set as the desktop background")]
+        public string windowName { get; set; }
+
+        [Option('p', "pid", HelpText = "Process ID holding the hwnd handle to set")]
+        public string pid { get; set; }
+    }
+
+    class StartOptions
+    {
+        [Option('p', "path", Required = true, HelpText = "Path to the executable to start")]
+        public string path { get; set; }
+
+        [Option('a', "args", Required = false, HelpText = "Command line arguments for the process to be created")]
+        public List<string> args { get; set; }
+    }
+
+    class ResetOptions
+    {
+        [Option('k', "kill", Required = false, HelpText = "Kill the processes after removing them from the wallpaper")]
+        public bool killProcess { get; set; }
+    }
+
+    class Options
+    {
+        public const string VERB_SET = "set";
+        public const string VERB_START = "start";
+        public const string VERB_RESET = "reset";
+
+        [HelpVerbOption]
+        public string GetUsage(string verb)
+        {
+            return CommandLine.Text.HelpText.AutoBuild(this, verb);
+        }
+
+        [VerbOption(VERB_RESET, HelpText = "Reset the desktop background to its default state")]
+        public ResetOptions ResetVerb { get; set; }
+
+        [VerbOption(VERB_SET, HelpText = "Set the desktop wallpaper from an existing window.")]
+        public SetOptions SetVerb { get; set; }
+
+        [VerbOption(VERB_START, HelpText = "Start a new process and set it as the desktop background")]
+        public StartOptions StartVerb { get; set; }
+
+    }
+}
--- wallcmd/Program.cs
+++ wallcmd/Program.cs
@@ -1,15 +1,60 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using Wallpainter;
 
 namespace wallcmd
 {
     class Program
     {
-        static void Main(string[] args)
+        static int Main(string[] args)
         {
+            string invokedVerb = "";
+            object invokedVerbOptions = null;
+            if (!CommandLine.Parser.Default.ParseArguments(args, new Options(),
+                (verb, verbOptions) =>
+                {
+                    invokedVerb = verb;
+                    invokedVerbOptions = verbOptions;
+                }))
+            {
+                return CommandLine.Parser.DefaultExitCodeFail;
+            }
+
+            switch (invokedVerb)
+            {
+                case Options.VERB_SET:
+                    Set((SetOptions)invokedVerbOptions);
+                    break;
+
+                case Options.VERB_START:
+                    Start((StartOptions)invokedVerbOptions);
+                    break;
+
+                case Options.VERB_RESET:
+                    Reset((ResetOptions)invokedVerbOptions);
+                    break;
+
+                default:
+                    return 1;
+            }
+
+            return 0;
+        }
+
+        public static void Set(SetOptions op)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static void Start(StartOptions op)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static void Reset(ResetOptions op)
+        {
+            WindowUtils.ClearWallpaper(op.killProcess);
         }
     }
 }
